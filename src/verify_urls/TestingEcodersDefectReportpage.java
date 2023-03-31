43910fe2a5d8740a1b0fe38689d3d20a15ed0874
G Karthik <G Karthik@SMTC-R-0128>
2023-03-31 13:23:02 +0530

    All the Urls added successfully.

---
+++ src/verify_urls/TestingEcodersDefectReportpage.java
@@ -0,0 +1,29 @@
+package verify_urls;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingEcodersDefectReportpage {
+
+	public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\G Karthik\\eclipse-workspace\\Testing_ecoders\\all_jar_files\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://127.0.0.1:5000/defectreport");
+        Thread.sleep(5000);
+        String s=driver.getTitle();
+        System.out.println(s);
+        String s1=driver.getCurrentUrl();
+        System.out.println(s1);
+        String s2=driver.getPageSource();
+        System.out.println(s2);
+        String expectedurl="http://127.0.0.1:5000/defectreport";
+        if(expectedurl.equals(s1)) {
+        	System.out.println("Testcase pass and  url matched");
+        }
+        else {
+        	System.out.println("Testcase failed and url didnot match");
+        }
+        driver.quit();
+	}
+
+}
